1815837d0e151ef97fbe3afa0abe1a87ffd4e807
noriokun4649 <noriokun4649@gmail.com>
2019-03-01 00:05:52 +0900

    画像。動画表示用Activity。内部にfragmentを持つ。

---
+++ app/src/main/java/jp/noriokun4649/noriotter2/activity/MediaActivity.java
@@ -0,0 +1,105 @@
+package jp.noriokun4649.noriotter2.activity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import jp.noriokun4649.noriotter2.R;
+import jp.noriokun4649.noriotter2.fragment.Media;
+
+public class MediaActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+    private int count;
+    private String[] url;
+
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.media_layout);
+        getWindow().setSharedElementsUseOverlay(true);
+        Intent intent = getIntent();
+        this.url = intent.getStringArrayExtra("urls");
+        count = url.length;
+        int index = intent.getIntExtra("index", 0);
+        IconicsDrawable gmdBack = new IconicsDrawable(this,
+                GoogleMaterial.Icon.gmd_arrow_back).sizeDp(30).paddingDp(6).color(Color.WHITE).backgroundColor(Color.argb(140, 0, 0, 0)).roundedCornersDp(15);
+        Toolbar toolbar = findViewById(R.id.toolbar3);
+        toolbar.setNavigationIcon(gmdBack);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                finishAfterTransition();
+            }
+        });
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = findViewById(R.id.media_view);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setCurrentItem(index);
+        mViewPager.setTransitionName("image" + index);
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(final int position) {
+                mViewPager.setTransitionName("image" + position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(final int state) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finishAfterTransition();
+                return true;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(final FragmentManager fm) {
+            super(fm);
+        }
+
+
+        @Override
+        public Fragment getItem(final int position) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("image_index", position);
+            bundle.putString("image_url", url[position]);
+            Media media = new Media();
+            media.setArguments(bundle);
+            return media;
+        }
+
+        @Override
+        public int getCount() {
+            return count;
+        }
+    }
+}
