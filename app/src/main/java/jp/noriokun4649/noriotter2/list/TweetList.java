4a6322268f13ab5d393a972a53b7b1a8e3d23f2b
noriokun4649 <noriokun4649@gmail.com>
2019-03-01 00:26:16 +0900

    ツイートを処理するリストView用アダプタ

---
+++ app/src/main/java/jp/noriokun4649/noriotter2/list/TweetList.java
@@ -0,0 +1,464 @@
+package jp.noriokun4649.noriotter2.list;
+
+/**
+ * Created by noriokun4649 on 2017/07/01.
+ */
+
+public class TweetList {
+    private boolean isLock;
+    private boolean isOfficial;
+    private String name;
+    private String screanname;
+    private String twiite;
+    private String location;
+    private String timestamp;
+    private long tweetid;
+    private String source;
+    private String imageURL;
+    private String[] urls;
+    private String[] medias;
+    private boolean isRetwet;
+    private String retwetOwner;
+    private int rtcount;
+    private int favocount;
+    private boolean isQuit;
+    private String quitText;
+    private String quitName;
+    private String quitScreenName;
+    private String[] quitMedias;
+    private String[] quitUrls;
+    private boolean meRt;
+    private boolean meFav;
+
+    /**
+     * Gets isLock .
+     *
+     * @return value of isLock
+     */
+    public boolean isLock() {
+        return isLock;
+    }
+
+    /**
+     * Sets isLock .
+     *
+     * @param lock set in lock
+     */
+    public void setLock(final boolean lock) {
+        isLock = lock;
+    }
+
+    /**
+     * Gets isOfficial .
+     *
+     * @return value of isOfficial
+     */
+    public boolean isOfficial() {
+        return isOfficial;
+    }
+
+    /**
+     * Sets isOfficial .
+     *
+     * @param official set in official
+     */
+    public void setOfficial(final boolean official) {
+        isOfficial = official;
+    }
+
+    /**
+     * Gets name .
+     *
+     * @return value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name .
+     *
+     * @param name set in name
+     */
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets screanname .
+     *
+     * @return value of screanname
+     */
+    public String getScreanname() {
+        return screanname;
+    }
+
+    /**
+     * Sets screanname .
+     *
+     * @param screanname set in screanname
+     */
+    public void setScreanname(final String screanname) {
+        this.screanname = screanname;
+    }
+
+    /**
+     * Gets twiite .
+     *
+     * @return value of twiite
+     */
+    public String getTwiite() {
+        return twiite;
+    }
+
+    /**
+     * Sets twiite .
+     *
+     * @param twiite set in twiite
+     */
+    public void setTwiite(final String twiite) {
+        this.twiite = twiite;
+    }
+
+    /**
+     * Gets location .
+     *
+     * @return value of location
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * Sets location .
+     *
+     * @param location set in location
+     */
+    public void setLocation(final String location) {
+        this.location = location;
+    }
+
+    /**
+     * Gets timestamp .
+     *
+     * @return value of timestamp
+     */
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * Sets timestamp .
+     *
+     * @param timestamp set in timestamp
+     */
+    public void setTimestamp(final String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    /**
+     * Gets tweetid .
+     *
+     * @return value of tweetid
+     */
+    public long getTweetid() {
+        return tweetid;
+    }
+
+    /**
+     * Sets tweetid .
+     *
+     * @param tweetid set in tweetid
+     */
+    public void setTweetid(final long tweetid) {
+        this.tweetid = tweetid;
+    }
+
+    /**
+     * Gets source .
+     *
+     * @return value of source
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     * Sets source .
+     *
+     * @param source set in source
+     */
+    public void setSource(final String source) {
+        this.source = source;
+    }
+
+    /**
+     * Gets imageURL .
+     *
+     * @return value of imageURL
+     */
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    /**
+     * Sets imageURL .
+     *
+     * @param imageURL set in imageURL
+     */
+    public void setImageURL(final String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    /**
+     * Gets urls .
+     *
+     * @return value of urls
+     */
+    public String[] getUrls() {
+        return urls;
+    }
+
+    /**
+     * Sets urls .
+     *
+     * @param urls set in urls
+     */
+    public void setUrls(final String[] urls) {
+        this.urls = urls;
+    }
+
+    /**
+     * Gets medias .
+     *
+     * @return value of medias
+     */
+    public String[] getMedias() {
+        return medias;
+    }
+
+    /**
+     * Sets medias .
+     *
+     * @param medias set in medias
+     */
+    public void setMedias(final String[] medias) {
+        this.medias = medias;
+    }
+
+    /**
+     * Gets isRetwet .
+     *
+     * @return value of isRetwet
+     */
+    public boolean isRetwet() {
+        return isRetwet;
+    }
+
+    /**
+     * Sets isRetwet .
+     *
+     * @param retwet set in retwet
+     */
+    public void setRetwet(final boolean retwet) {
+        isRetwet = retwet;
+    }
+
+    /**
+     * Gets retwetOwner .
+     *
+     * @return value of retwetOwner
+     */
+    public String getRetwetOwner() {
+        return retwetOwner;
+    }
+
+    /**
+     * Sets retwetOwner .
+     *
+     * @param retwetOwner set in retwetOwner
+     */
+    public void setRetwetOwner(final String retwetOwner) {
+        this.retwetOwner = retwetOwner;
+    }
+
+    /**
+     * Gets rtcount .
+     *
+     * @return value of rtcount
+     */
+    public int getRtcount() {
+        return rtcount;
+    }
+
+    /**
+     * Sets rtcount .
+     *
+     * @param rtcount set in rtcount
+     */
+    public void setRtcount(final int rtcount) {
+        this.rtcount = rtcount;
+    }
+
+    /**
+     * Gets favocount .
+     *
+     * @return value of favocount
+     */
+    public int getFavocount() {
+        return favocount;
+    }
+
+    /**
+     * Sets favocount .
+     *
+     * @param favocount set in favocount
+     */
+    public void setFavocount(final int favocount) {
+        this.favocount = favocount;
+    }
+
+    /**
+     * Gets isQuit .
+     *
+     * @return value of isQuit
+     */
+    public boolean isQuit() {
+        return isQuit;
+    }
+
+    /**
+     * Sets isQuit .
+     *
+     * @param quit set in quit
+     */
+    public void setQuit(final boolean quit) {
+        isQuit = quit;
+    }
+
+    /**
+     * Gets quitText .
+     *
+     * @return value of quitText
+     */
+    public String getQuitText() {
+        return quitText;
+    }
+
+    /**
+     * Sets quitText .
+     *
+     * @param quitText set in quitText
+     */
+    public void setQuitText(final String quitText) {
+        this.quitText = quitText;
+    }
+
+    /**
+     * Gets quitName .
+     *
+     * @return value of quitName
+     */
+    public String getQuitName() {
+        return quitName;
+    }
+
+    /**
+     * Sets quitName .
+     *
+     * @param quitName set in quitName
+     */
+    public void setQuitName(final String quitName) {
+        this.quitName = quitName;
+    }
+
+    /**
+     * Gets quitScreenName .
+     *
+     * @return value of quitScreenName
+     */
+    public String getQuitScreenName() {
+        return quitScreenName;
+    }
+
+    /**
+     * Sets quitScreenName .
+     *
+     * @param quitScreenName set in quitScreenName
+     */
+    public void setQuitScreenName(final String quitScreenName) {
+        this.quitScreenName = quitScreenName;
+    }
+
+    /**
+     * Gets quitMedias .
+     *
+     * @return value of quitMedias
+     */
+    public String[] getQuitMedias() {
+        return quitMedias;
+    }
+
+    /**
+     * Sets quitMedias .
+     *
+     * @param quitMedias set in quitMedias
+     */
+    public void setQuitMedias(final String[] quitMedias) {
+        this.quitMedias = quitMedias;
+    }
+
+    /**
+     * Gets quitUrls .
+     *
+     * @return value of quitUrls
+     */
+    public String[] getQuitUrls() {
+        return quitUrls;
+    }
+
+    /**
+     * Sets quitUrls .
+     *
+     * @param quitUrls set in quitUrls
+     */
+    public void setQuitUrls(final String[] quitUrls) {
+        this.quitUrls = quitUrls;
+    }
+
+    /**
+     * Gets meRt .
+     *
+     * @return value of meRt
+     */
+    public boolean isMeRt() {
+        return meRt;
+    }
+
+    /**
+     * Sets meRt .
+     *
+     * @param meRt set in meRt
+     */
+    public void setMeRt(final boolean meRt) {
+        this.meRt = meRt;
+    }
+
+    /**
+     * Gets meFav .
+     *
+     * @return value of meFav
+     */
+    public boolean isMeFav() {
+        return meFav;
+    }
+
+    /**
+     * Sets meFav .
+     *
+     * @param meFav set in meFav
+     */
+    public void setMeFav(final boolean meFav) {
+        this.meFav = meFav;
+    }
+}
---
+++ app/src/main/java/jp/noriokun4649/noriotter2/list/TweetListItemAdapter.java
@@ -0,0 +1,205 @@
+package jp.noriokun4649.noriotter2.list;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.text.Html;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import jp.noriokun4649.noriotter2.R;
+import jp.noriokun4649.noriotter2.glide.MyGlideApp;
+import jp.noriokun4649.noriotter2.twitter.TwitterConnect;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+/**
+ * Created by noriokun4649 on 2017/07/01.
+ */
+
+public class TweetListItemAdapter extends ArrayAdapter<TweetList> {
+    private final LayoutInflater inflater;
+    private List<TweetList> arrayList;
+    private Context context;
+    private TwitterConnect twitterIn;
+
+    public TweetListItemAdapter(final Context context, final List<TweetList> objects) {
+        super(context, 0, objects);
+        inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+        arrayList = objects;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(final int position, final View convertView, final ViewGroup parent) {
+        final View view = inflater.inflate(R.layout.tl_list_layout, null, false);
+        View.OnClickListener clickListener = (View v) -> ((ListView) parent).performItemClick(v, position, v.getId());
+        TextView textView6 = view.findViewById(R.id.textView6);
+        TextView textView = view.findViewById(R.id.textView);
+        TextView textView7 = view.findViewById(R.id.textView7);
+        TextView textView8 = view.findViewById(R.id.textView8);
+        TextView textView10 = view.findViewById(R.id.textView10);
+        TextView textView14 = view.findViewById(R.id.textView14);
+        TextView textView32 = view.findViewById(R.id.textView32);
+        TextView textView33 = view.findViewById(R.id.textView33);
+        TextView textView34 = view.findViewById(R.id.textView34);
+        TextView textView35 = view.findViewById(R.id.textView35);
+        TextView textView36 = view.findViewById(R.id.textView36);
+        ImageView image17 = view.findViewById(R.id.image1);
+        ImageView image18 = view.findViewById(R.id.image2);
+        ImageView image19 = view.findViewById(R.id.image3);
+        ImageView image20 = view.findViewById(R.id.image4);
+        ImageView isLock = view.findViewById(R.id.imageView13);
+        ImageView isOfficial = view.findViewById(R.id.imageView14);
+        VideoView video = view.findViewById(R.id.videoView);
+        ConstraintLayout constraintLayout = view.findViewById(R.id.image_layout);
+        final ImageView imageView8 = view.findViewById(R.id.imageView8);
+        final TweetList useredata = arrayList.get(getCount() - 1 - position);
+        if (useredata.getLocation().equals("")) {
+            textView14.setVisibility(View.GONE);
+        } else {
+            textView14.setVisibility(View.VISIBLE);
+        }
+        if (useredata.getUrls() != null) {
+            String[] urls = useredata.getUrls();
+            switch (urls.length) {
+                case 4:
+                    Log.d("URL", urls[3]);
+                case 3:
+                    Log.d("URL", urls[2]);
+                case 2:
+                    Log.d("URL", urls[1]);
+                case 1:
+                    Log.d("URL", urls[0]);
+                default:
+            }
+        }
+        if (useredata.getMedias() != null) {
+            mediaTweet(clickListener, useredata.getMedias(), constraintLayout, video, view, image17, image18, image19, image20);
+        }
+        if (useredata.isRetwet()) {
+            textView32.setVisibility(View.VISIBLE);
+            textView32.setText("{cmd-twitter-retweet}  " + useredata.getRetwetOwner() + "さんがリツイートしました");
+            textView32.setOnClickListener(clickListener);
+        }
+        if (useredata.isQuit()) {
+            LinearLayout quitLine = view.findViewById(R.id.quit_line);
+            quitLine.setVisibility(View.VISIBLE);
+            quitLine.setOnClickListener(clickListener);
+            TextView quitText = view.findViewById(R.id.quit_text);
+            TextView quitName = view.findViewById(R.id.quit_name);
+            TextView quitScreanname = view.findViewById(R.id.quit_screanname);
+            quitText.setText(useredata.getQuitText());
+            quitName.setText(useredata.getQuitName());
+            quitScreanname.setText("@" + useredata.getQuitScreenName());
+            if (useredata.getQuitMedias() != null) {
+                ConstraintLayout quitImageLayout = view.findViewById(R.id.quit_image_layout);
+                quitImageLayout.setVisibility(View.VISIBLE);
+                VideoView quitVideo = view.findViewById(R.id.quit_videoView);
+                ImageView quitImage1 = view.findViewById(R.id.quit_image1);
+                ImageView quitImage2 = view.findViewById(R.id.quit_image2);
+                ImageView quitImage3 = view.findViewById(R.id.quit_image3);
+                ImageView quitImage4 = view.findViewById(R.id.quit_image4);
+                mediaTweet(clickListener, useredata.getQuitMedias(), quitImageLayout, quitVideo, view, quitImage1, quitImage2, quitImage3, quitImage4);
+            }
+        }
+        textView33.setText("{cmd-message}");
+        textView33.setOnClickListener(clickListener);
+        textView34.setText("{cmd-twitter-retweet}" + useredata.getRtcount());
+        textView34.setOnClickListener(clickListener);
+        textView34.setTextColor(useredata.isMeRt() ? Color.GREEN : Color.BLACK);
+        textView35.setText("{gmd-favorite}" + useredata.getFavocount());
+        textView35.setOnClickListener(clickListener);
+        textView35.setTextColor(useredata.isMeFav() ? Color.RED : Color.BLACK);
+        textView36.setText("{gmd-share}");
+        textView36.setOnClickListener(clickListener);
+        textView.setText("@" + useredata.getScreanname());
+        textView6.setText(useredata.getName());
+        textView7.setText(useredata.getTwiite());
+        textView8.setText(Html.fromHtml(useredata.getSource()));
+        textView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                String in = useredata.getSource();
+                final Pattern urlPattern = Pattern.compile("(http://|https://){1}[\\w\\.\\-/:\\#\\?\\=\\&\\;\\%\\~\\+]+", Pattern.CASE_INSENSITIVE);
+                final Matcher matcher = urlPattern.matcher(useredata.getSource());
+                while (matcher.find()) {
+                    System.out.println(matcher.group());
+                    Uri uri = Uri.parse(matcher.group());
+                    Intent i = new Intent(Intent.ACTION_VIEW, uri);
+                    context.startActivity(i);
+                }
+            }
+        });
+        if (useredata.isLock()) {
+            isLock.setVisibility(View.VISIBLE);
+        } else {
+            isLock.setVisibility(View.INVISIBLE);
+        }
+        if (useredata.isOfficial()) {
+            isOfficial.setVisibility(View.VISIBLE);
+        } else {
+            isOfficial.setVisibility(View.INVISIBLE);
+        }
+        textView10.setText(useredata.getTimestamp());
+        textView14.setText(useredata.getLocation());
+        final Uri uri = Uri.parse(useredata.getImageURL());
+        MyGlideApp.with(view).load(uri).diskCacheStrategy(DiskCacheStrategy.ALL).circleCrop().into(imageView8);
+
+        return view;
+    }
+
+    private void mediaTweet(final View.OnClickListener clickListener, final String[] media,
+                            final ConstraintLayout constraintLayout,
+                            final VideoView video, final View mainView, final ImageView image1,
+                            final ImageView image2, final ImageView image3, final ImageView image4) {
+        if (media[0].contains(".mp4")) {
+            constraintLayout.setVisibility(View.GONE);
+            video.setVisibility(View.VISIBLE);
+            video.setVideoURI(Uri.parse(media[0]));
+            video.setOnClickListener(clickListener);
+        } else {
+            constraintLayout.setVisibility(View.VISIBLE);
+            video.setVisibility(View.GONE);
+            switch (media.length) {
+                case 4:
+                    image4.setVisibility(View.VISIBLE);
+                    image4.setOnClickListener(clickListener);
+                    Glide.with(mainView).load(media[3]).diskCacheStrategy(DiskCacheStrategy.ALL).into(image4);
+                case 3:
+                    image3.setVisibility(View.VISIBLE);
+                    image3.setOnClickListener(clickListener);
+                    Glide.with(mainView).load(media[2]).diskCacheStrategy(DiskCacheStrategy.ALL).into(image3);
+                case 2:
+                    image2.setVisibility(View.VISIBLE);
+                    image2.setOnClickListener(clickListener);
+                    Glide.with(mainView).load(media[1]).diskCacheStrategy(DiskCacheStrategy.ALL).into(image2);
+                case 1:
+                    image1.setVisibility(View.VISIBLE);
+                    image1.setOnClickListener(clickListener);
+                    Glide.with(mainView).load(media[0]).diskCacheStrategy(DiskCacheStrategy.ALL).into(image1);
+                    break;
+                default:
+                    constraintLayout.setVisibility(View.GONE);
+            }
+        }
+    }
+
+}
