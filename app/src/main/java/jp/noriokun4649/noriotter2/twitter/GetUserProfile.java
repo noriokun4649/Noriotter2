7a1d492a9359484d55880af3392fd861de7765cd
noriokun4649 <noriokun4649@gmail.com>
2019-03-04 03:01:23 +0900

    プロフィール画面追加

---
+++ app/src/main/java/jp/noriokun4649/noriotter2/twitter/GetUserProfile.java
@@ -0,0 +1,21 @@
+package jp.noriokun4649.noriotter2.twitter;
+
+import android.app.Activity;
+
+import twitter4j.AsyncTwitter;
+
+public class GetUserProfile {
+
+    private AsyncTwitter asyncTwitter;
+
+    public GetUserProfile(final Activity contexts, final AsyncTwitter asyncTwitterd, final StatusCallBack callBack) {
+        this.asyncTwitter = asyncTwitterd;
+        asyncTwitterd.addListener(new TimeLineTwetterAdapter(callBack, asyncTwitterd));
+
+    }
+
+    public void getUserProfile(final String id) {
+        asyncTwitter.showUser(id);
+    }
+
+}
